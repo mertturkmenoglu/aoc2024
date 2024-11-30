11a71e6b9ba98ae84c7eec831c74104647775e78
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-01 00:08:55 +0300

    add example day

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import module from "@/app/day01";
+import { formatTime, measure } from "@/lib";
+
+function main() {
+  const [res1, dur1] = measure(module.sol1);
+  const [res2, dur2] = measure(module.sol2);
+
+  console.info("Day:", module.day);
+  console.table([
+    {
+      name: "Solution 1",
+      result: res1,
+      expected: module.exp1,
+      duration: formatTime(dur1),
+      correct: res1 === module.exp1,
+    },
+    {
+      name: "Solution 2",
+      result: res2,
+      expected: module.exp2,
+      duration: formatTime(dur2),
+      correct: res2 === module.exp2,
+    },
+  ]);
+}
+
+main();

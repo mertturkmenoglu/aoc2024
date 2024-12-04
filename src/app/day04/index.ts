c9542ed8e5e9f398e77ea0ae9ca8af5ccd183a5c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-04 10:00:36 +0300

    make it even more unreadable

--- src/app/day04/index.ts
+++ src/app/day04/index.ts
@@ -2,73 +2,39 @@ import { defineAocModule, readLines, diagonalCoefs as coefs } from "@/lib";
 
 const lines: string[] = readLines("day04/input.txt");
 const g: string[][] = lines.map((l) => l.split(""));
+const A = () => new Array(lines.length).fill(0);
 
-function searchLine(line: string): number {
+function s(line: string): number {
   const [a, b] = [/XMAS/g, /SAMX/g].map((r) => [...line.matchAll(r)].length);
   return a + b;
 }
 
-function checkDiagonals(r: number, c: number): number {
-  return coefs.filter(
-    ([a, b]) =>
-      g[r][c] +
-        g[r + a]?.[c + b] +
-        g[r + 2 * a]?.[c + 2 * b] +
-        g[r + 3 * a]?.[c + 3 * b] ===
-      "XMAS",
-  ).length;
+function c1(r: number, c: number): number {
+  let _ = [0, 1, 2, 3];
+  return coefs
+    .map(([a, b]) => _.map((x) => g[r + x * a]?.[c + x * b]).join(""))
+    .filter((x) => x === "XMAS").length;
 }
 
-function check2(r: number, c: number): number {
-  let count = 0;
-  const el = g[r][c];
-
-  if (el !== "M" && el !== "S") {
-    return 0;
-  }
-
-  const comp = el === "M" ? "S" : "M";
-
-  if (g[r + 1]?.[c + 1] === "A" && g[r + 2]?.[c + 2] === comp) {
-    const val = g[r + 2]?.[c] + g[r + 1]?.[c + 1] + g[r]?.[c + 2];
-    if (val === "SAM" || val === "MAS") {
-      count++;
-    }
-  }
-
-  return count;
+function c2(r: number, c: number): number {
+  let a = g[r + 1]?.[c + 1];
+  let b = "MS".includes(g[r][c]) && a === "A";
+  let d = g[r + 2]?.[c + 2] === (g[r][c] === "M" ? "S" : "M");
+  let e = b && d && ["SAM", "MAS"].includes(g[r + 2]?.[c] + a + g[r]?.[c + 2]);
+  return e ? 1 : 0;
 }
 
 function sol1(): number {
-  let count = 0;
-
-  for (let i = 0; i < lines.length; i++) {
-    count += searchLine(lines[i]);
-  }
-
-  for (let i = 0; i < g[0].length; i++) {
-    count += searchLine(g.map((row) => row[i]).join(""));
-  }
-
-  for (let i = 0; i < lines.length; i++) {
-    for (let j = 0; j < lines[i].length; j++) {
-      count += checkDiagonals(i, j);
-    }
-  }
-
-  return count;
+  return lines.reduce((acc, l, i) => {
+    acc += s(l) + s(g.map((row) => row[i]).join(""));
+    return acc + A().reduce((acc, _, j) => acc + c1(i, j), 0);
+  }, 0);
 }
 
 function sol2(): number {
-  let count = 0;
-
-  for (let i = 0; i < lines.length; i++) {
-    for (let j = 0; j < lines[i].length; j++) {
-      count += check2(i, j);
-    }
-  }
-
-  return count;
+  return A().reduce((I, _, i) => {
+    return I + A().reduce((J, _, j) => J + c2(i, j), 0);
+  }, 0);
 }
 
 export default defineAocModule({

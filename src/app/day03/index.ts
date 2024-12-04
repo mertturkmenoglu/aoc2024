571f722c349c1933b3cc481f062571ee9e6ce244
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-04 14:18:31 +0300

    mooooooooore

--- src/app/day03/index.ts
+++ src/app/day03/index.ts
@@ -1,25 +1,16 @@
-import { defineAocModule, readLines } from "@/lib";
+import { defineAocModule, readLines, sum } from "@/lib";
 
-let [L, rg] = [readLines("day03/input.txt"), /mul\(\d+,\d+\)|do\(\)|don\'t\(\)/g];
-let M = (s: string) => s.slice(4, -1).split(",").map(Number);
-let N = (s: string) => M(s).reduce((acc, x) => acc * x, 1);
-let I = (s: string) => s.startsWith("mul");
-
-function compute(line: string, v: boolean): number {
-  let [e, c] = [true, 0];
-
-  for (const [s] of line.matchAll(rg)) {
-    e = !I(s) ? v || s === "do()" : e;
-    c += e && I(s) ? N(s) : 0;
-  }
-
-  return c;
-}
+let [L, rg, M] = [readLines("day03/input.txt"), /mul\(\d+,\d+\)|do\(\)|don\'t\(\)/g, (s: string) => s.slice(4, -1).split(",").map(Number)];
+let [N, I] = [(s: string) => M(s).reduce((acc, x) => acc * x, 1), (s: string) => s.startsWith("mul")];
+let A = (l: string) => [...l.matchAll(rg)].map((x) => x[0]);
+let [B, F] = [(s: string) => (I(s) ? N(s) : 0), (s: string, e: boolean) => (!I(s) ? s === "do()" : e)];
+let C = (l: string) => sum(A(l).map((x) => B(x)));
+let D = (l: string) => A(l).reduce(({ e, c }, s) => ({ e: F(s, e), c: c + (F(s, e) ? B(s) : 0) }), { e: true, c: 0 });
 
 export default defineAocModule({
   day: 3,
   exp1: 182_619_815,
   exp2: 80_747_545,
-  sol1: () => compute(L.join(""), true),
-  sol2: () => compute(L.join(""), false),
+  sol1: () => C(L.join("")),
+  sol2: () => D(L.join("")).c,
 });

92f5aae09683b340227d26a6f18545fe7c7cd621
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-04 09:07:10 +0300

    add permutation utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,3 +13,29 @@ export function readLines(path: string, relative: boolean = true): string[] {
 export function formatTime(t: number): string {
   return `${t.toFixed(4)} milliseconds`;
 }
+
+export function permutation<T>(arr: T[]): T[][] {
+  const all: T[][] = [];
+
+  function heapPermutation(a: T[], size: number, n: number) {
+    if (size == 1) all.push(a.slice(0, n));
+
+    for (let i = 0; i < size; i++) {
+      heapPermutation(a, size - 1, n);
+
+      if (size % 2 == 1) {
+        let temp = a[0];
+        a[0] = a[size - 1];
+        a[size - 1] = temp;
+      } else {
+        let temp = a[i];
+        a[i] = a[size - 1];
+        a[size - 1] = temp;
+      }
+    }
+  }
+
+  heapPermutation(arr, arr.length, arr.length);
+
+  return all;
+}

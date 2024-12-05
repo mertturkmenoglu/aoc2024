d47b82d6553d226ee366fd18be9aa360b1fb3520
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-05 10:25:03 +0300

    obfuscate day 5 codes

--- src/app/day05/index.ts
+++ src/app/day05/index.ts
@@ -1,127 +1,22 @@
-import { defineAocModule, permutation, readLines } from "@/lib";
+import { defineAocModule, permutation, readLines, sum } from "@/lib";
 
-type Rule = [number, number];
-type Update = number[];
+type V = Map<number, number>;
 
-type Data = {
-  rules: Rule[];
-  updates: Update[];
-};
-
-function parseInput(): Data {
-  const emptyLineIndex = lines.findIndex((line) => line.length === 0);
-  const rulesArr: Rule[] = [];
-  const updatesArr: Update[] = [];
-
-  for (let i = 0; i < emptyLineIndex; i++) {
-    const line = lines[i];
-    const [a, b] = line.split("|");
-    rulesArr.push([+a, +b]);
-  }
-
-  for (let i = emptyLineIndex + 1; i < lines.length; i++) {
-    const line = lines[i];
-    updatesArr.push(line.split(",").map(Number));
-  }
-
-  return {
-    rules: rulesArr,
-    updates: updatesArr,
-  };
-}
-
-const lines: string[] = readLines("day05/input.txt");
-const { rules, updates }: Data = parseInput();
-
-function isCorrect(update: Update): boolean {
-  for (const el of update) {
-    const relatedRules = rules.filter((r) => r[0] === el || r[1] === el);
-
-    for (const [a, b] of relatedRules) {
-      const ai = update.indexOf(a);
-      const bi = update.indexOf(b);
-
-      if (ai === -1 || bi === -1) {
-        continue;
-      }
-
-      if (ai >= bi) {
-        return false;
-      }
-    }
-  }
-
-  return true;
-}
-
-function order(update: Update): Update {
-  const rr: Rule[] = [];
-
-  for (const el of update) {
-    const related = rules.filter((r) => r[0] === el || r[1] === el);
-    rr.push(...related);
-  }
-
-  // element - how many rules
-  const freq = new Map<number, number>();
-
-  for (const r of rr) {
-    const el = r[0];
-    if (update.indexOf(el) === -1) {
-      continue;
-    }
-    const c = freq.get(el);
-    const has = c !== undefined;
-
-    if (has) {
-      freq.set(el, c + 1);
-    } else {
-      freq.set(el, 1);
-    }
-  }
-
-  // array of element - count
-  const entries = [...freq.entries()];
-  entries.sort((a, b) => b[1] - a[1]);
-  return entries.map((x) => x[0]);
-}
-
-function middle(update: Update): number {
-  const i = Math.floor(update.length / 2);
-  return update[i];
-}
-
-function sol1(): number {
-  let sum = 0;
-
-  for (const update of updates) {
-    if (isCorrect(update)) {
-      sum += middle(update);
-    }
-  }
-
-  return sum;
-}
-
-function sol2(): number {
-  let sum = 0;
-
-  for (let i = 0; i < updates.length; i++) {
-    const update = updates[i];
-
-    if (!isCorrect(update)) {
-      const corrected = order(update);
-      sum += middle(corrected);
-    }
-  }
-
-  return sum;
-}
+let [L, A] = [readLines("day05/input.txt"), (n: number) => [...new Array(n).keys()]];
+let [E, re] = [L.findIndex((l) => l.length === 0), (u: number[]) => J(u).filter((r) => u.indexOf(r[0]) !== -1)];
+let R = A(E).map((i) => L[i].split("|").map(Number) as [number, number]);
+let [U, F] = [A(L.length - E - 1).map((i) => L[i + E + 1].split(",").map(Number)), (): V => new Map()];
+let [M, Q] = [(u: number[]) => u[Math.floor(u.length / 2)], (f: V, Y: number) => f.set(Y, f.has(Y) ? f.get(Y)! + 1 : 1)];
+let J = (u: number[]) => u.map((Y) => R.filter((r) => r[0] === Y || r[1] === Y)).flat();
+let [W, T] = [(Y: number) => R.filter((r) => r.some((x) => x === Y)), (r: [number, number], u: number[]) => r.map((x) => u.indexOf(x))];
+let G = (f: V) => [...f.entries()].toSorted((a, b) => b[1] - a[1]).map((x) => x[0]);
+let C = (u: number[]) => !u.some((el) => W(el).some((r) => [T(r, u)].map((a) => a[0] >= a[1] && a.every((x) => x !== -1))[0]));
+let O = (u: number[]) => G([F()].map((f) => re(u).map(([el]) => Q(f, el))[0])[0]);
 
 export default defineAocModule({
   day: 5,
   exp1: 6505,
   exp2: 6897,
-  sol1,
-  sol2,
+  sol1: () => sum(U.filter((u) => C(u)).map((u) => M(u))),
+  sol2: () => sum(U.filter((u) => !C(u)).map((u) => M(O(u)))),
 });

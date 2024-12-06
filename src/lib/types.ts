9666b193d5ab9b1d0203cfb14b6ca0c9ece70ac2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-07 00:28:18 +0300

    add utility types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,23 @@
-export type AocModule = {
-  day: number;
-  exp1: number;
-  exp2: number;
-  sol1: () => number;
-  sol2: () => number;
-};
+/**
+ * 2 dimensional array type alias.
+ */
+export type Matrix<T> = T[][];
 
-export function defineAocModule(def: AocModule): AocModule {
-  return def;
-}
+/**
+ * A type alias for a 1-tuple.
+ */
+export type Singleton<T> = [T];
+
+/**
+ * A type alias for a 2-tuple.
+ *
+ * It is short for "Ordered Pair"
+ */
+export type Pair<T, U> = [T, U];
+
+/**
+ * A 2-tuple to represent a position on a matrix/grid.
+ *
+ * It's a generic type alias for number 2-tuples.
+ */
+export type Pos = [number, number];

3c7486d374afe3fb2dace8c0bdbfa9bdf8b9e3df
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-06 10:22:46 +0300

    the phaaantom of the opera is there inside my miiiiindddd

--- src/app/day06/index.ts
+++ src/app/day06/index.ts
@@ -1,35 +1,22 @@
 import { defineAocModule, readLines, cardinalCoefs as C } from "@/lib";
 
-type Pos = [number, number];
-type Dir = "N" | "E" | "S" | "W";
-type Grid = string[][];
+type P = [number, number];
+type R = "N" | "E" | "S" | "W";
+type H = string[][];
 
-let L = readLines("day06/input.txt");
-let g = L.map((l) => l.split(""));
-let [N] = [L.length];
-let I = ([r, c]: Pos) => !(r < 0 || c < 0 || r >= N || c >= N);
-let O = ([r, c]: Pos, dir: Dir, G: Grid) => [GD(dir)].every(([dr, dc]) => G[r + dr]?.[c + dc] === "#");
-let GD = (dir: Dir) => C[[3, 0, 1, 2][DIRS.indexOf(dir)]];
-let DIRS: Dir[] = ["N", "E", "S", "W"];
-let ND = (d: Dir) => DIRS[(DIRS.indexOf(d) + 1) % 4];
-let A = () => [...new Array(N).keys()];
-let sr = g.findIndex((r) => r.includes("^"));
-let sp = [sr, g[sr].indexOf("^")];
-let S = JSON.stringify;
-let c = ([i, j]: Pos) => (S([i, j]) !== S(sp) ? compute([...g.map((x, ii) => x.map((h, jj) => (ii === i && jj === j ? "#" : h)))]) : -2);
+let [L, S, D] = [readLines("day06/input.txt"), JSON.stringify, ["N", "E", "S", "W"] as R[]];
+let [g, N, A] = [L.map((l) => l.split("")), L.length, () => [...new Array(N).keys()]];
+let [I, GD] = [([r, c]: P) => !(r < 0 || c < 0 || r >= N || c >= N), (dir: R) => C[[3, 0, 1, 2][D.indexOf(dir)]]];
+let [O, ND] = [([r, c]: P, d: R, G: H) => [GD(d)].every(([dr, dc]) => G[r + dr]?.[c + dc] === "#"), (d: R) => D[(D.indexOf(d) + 1) % 4]];
+let sp = [g.findIndex((r) => r.includes("^")), g[g.findIndex((r) => r.includes("^"))].indexOf("^")];
+let c = ([i, j]: P) => (S([i, j]) !== S(sp) ? compute([...g.map((x, ii) => x.map((h, jj) => (ii === i && jj === j ? "#" : h)))]) : -2);
 
-let compute = (G: Grid) => {
-  let [[r, c], dr, dc, i, dir, v] = [sp, -1, 0, 0, "N" as Dir, new Map<string, Pos>()];
-  while (I([r, c])) {
-    if (i++ > 6_000) return -1;
-    v.set(S([r, c]), [r, c]);
-    if (O([r, c], dir, G)) {
-      [dir, dr, dc] = [ND(dir), GD(ND(dir))[0], GD(ND(dir))[1]];
-    } else {
-      [r, c] = [r + dr, c + dc];
-    }
-  }
-  return v.size;
+let compute = (G: H) => {
+  let [[r, c], dr, dc, i, dir, v] = [sp, -1, 0, 0, "N" as R, new Map<string, P>()];
+  while (I([r, c]) && i++ < 6_000 && v.set(S([r, c]), [r, c]))
+    if (O([r, c], dir, G)) [dir, dr, dc] = [ND(dir), GD(ND(dir))[0], GD(ND(dir))[1]];
+    else [r, c] = [r + dr, c + dc];
+  return i > 6000 ? -1 : v.size;
 };
 
 export default defineAocModule({

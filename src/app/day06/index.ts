f2483a2137036a95e53ab186735a33912fcbf52e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-06 09:23:15 +0300

    uglify

--- src/app/day06/index.ts
+++ src/app/day06/index.ts
@@ -1,12 +1,22 @@
-import { defineAocModule, readLines, cardinalCoefs } from "@/lib";
+import { defineAocModule, readLines, cardinalCoefs as C } from "@/lib";
 
-const lines: string[] = readLines("day06/input.txt");
-const grid: string[][] = lines.map((l) => l.split(""));
+type Pos = [number, number];
+type Dir = "N" | "E" | "S" | "W";
+type Grid = string[][];
 
-function findStartPosition(): [number, number] {
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] === "^") {
+let L = readLines("day06/input.txt");
+let g = L.map((l) => l.split(""));
+let I = ([r, c]: Pos) => !(r < 0 || c < 0 || r >= g.length || c >= g[0].length);
+let O = ([r, c]: Pos, dir: Dir) => [getDeltaForDir(dir)].every(([dr, dc]) => g[r + dr]?.[c + dc] === "#");
+let getDeltaForDir = (dir: Dir) => C[[3, 0, 1, 2][DIRS.indexOf(dir)]];
+let DIRS: Dir[] = ["N", "E", "S", "W"];
+let getNewDir = (d: Dir) => DIRS[(DIRS.indexOf(d) + 1) % DIRS.length];
+let sp = findStartPosition();
+
+function findStartPosition(): Pos {
+  for (let i = 0; i < g.length; i++) {
+    for (let j = 0; j < g[i].length; j++) {
+      if (g[i][j] === "^") {
         return [i, j];
       }
     }
@@ -15,61 +25,12 @@ function findStartPosition(): [number, number] {
   throw new Error("Cannot find starting position");
 }
 
-type Pos = [number, number];
-type Dir = "N" | "E" | "S" | "W";
-
-function isInGrid([r, c]: Pos, g: string[][]): boolean {
-  if (r < 0 || c < 0) return false;
-  if (r >= g.length || c >= g[0].length) return false;
-  return true;
-}
-
-function isObstacleFront([r, c]: Pos, dir: Dir, g: string[][]): boolean {
-  let [dr, dc] = getDeltaForDir(dir);
-  let el = g[r + dr]?.[c + dc];
-  return el === "#";
-}
-
-function getDeltaForDir(dir: Dir): [number, number] {
-  let cc = cardinalCoefs[0];
-
-  if (dir === "N") {
-    cc = cardinalCoefs[3];
-  } else if (dir === "E") {
-    cc = cardinalCoefs[0];
-  } else if (dir === "S") {
-    cc = cardinalCoefs[1];
-  } else {
-    cc = cardinalCoefs[2];
-  }
-
-  return cc;
-}
-
-function getNewDir(dir: Dir): Dir {
-  switch (dir) {
-    case "N":
-      return "E";
-    case "E":
-      return "S";
-    case "S":
-      return "W";
-    case "W":
-      return "N";
-    default:
-      throw new Error("unreachable");
-  }
-}
-
-function compute(startPos: [number, number], g: string[][]): number {
-  const visited = new Map<string, Pos>();
-  let [r, c] = startPos;
-  let dr = -1;
-  let dc = 0;
+function compute(): number {
+  let visited = new Map<string, Pos>();
+  let [[r, c], dr, dc, i] = [sp, -1, 0, 0];
   let dir: Dir = "N";
-  let i = 0;
 
-  while (isInGrid([r, c], g)) {
+  while (I([r, c])) {
     if (i > 6_000) {
       return -1;
     }
@@ -78,7 +39,7 @@ function compute(startPos: [number, number], g: string[][]): number {
     visited.set(`${r}-${c}`, [r, c]);
 
     // is there an obstacle directly in front of us?
-    if (isObstacleFront([r, c], dir, g)) {
+    if (O([r, c], dir)) {
       dir = getNewDir(dir);
       let delta = getDeltaForDir(dir);
       dr = delta[0];
@@ -93,12 +54,12 @@ function compute(startPos: [number, number], g: string[][]): number {
   return visited.size;
 }
 
-function compute2([r, c]: Pos, g: string[][]): number {
+function compute2(): number {
   let counter = 0;
 
   for (let i = 0; i < g.length; i++) {
     for (let j = 0; j < g[0].length; j++) {
-      if (i === r && c === j) {
+      if (i === sp[0] && j === sp[1]) {
         continue;
       }
 
@@ -107,7 +68,7 @@ function compute2([r, c]: Pos, g: string[][]): number {
       g[i][j] = "#";
 
       // call compute
-      let path = compute([r, c], g);
+      let path = compute();
 
       // is in loop
       if (path === -1) {
@@ -122,22 +83,10 @@ function compute2([r, c]: Pos, g: string[][]): number {
   return counter;
 }
 
-function sol1(): number {
-  const sp = findStartPosition();
-  const res = compute(sp, grid);
-  return res;
-}
-
-function sol2(): number {
-  const sp = findStartPosition();
-  const res = compute2(sp, grid);
-  return res;
-}
-
 export default defineAocModule({
   day: 6,
   exp1: 5030,
   exp2: 1928,
-  sol1,
-  sol2,
+  sol1: compute,
+  sol2: compute2,
 });

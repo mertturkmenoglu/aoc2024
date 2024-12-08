15c7503169f29c351abd3c0bc3160079b8f68002
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-08 08:53:01 +0300

    make it better

--- src/app/day08/index.ts
+++ src/app/day08/index.ts
@@ -1,4 +1,4 @@
-import { defineAocModule, readLines, type Pos } from "@/lib";
+import { defineAocModule, posAdd, posSub, readLines, type Pos } from "@/lib";
 import { Mtr } from "@/lib";
 
 const lines: string[] = readLines("day08/input.txt");
@@ -6,23 +6,19 @@ const m: string[][] = lines.map((l) => l.split(""));
 
 type PosString = string;
 
+const Key = JSON.stringify;
+
 function findAntennas(): Map<string, Pos[]> {
   const antennas = new Map<string, Pos[]>();
   const [r, c] = Mtr.dims(m);
 
-  // Find antennas in the grid.
   for (let i = 0; i < r; i++) {
     for (let j = 0; j < c; j++) {
       let ch = Mtr.at(m, [i, j]);
       if (ch !== ".") {
         const arr = antennas.get(ch);
         const has = arr !== undefined;
-
-        if (has) {
-          antennas.set(ch, [...arr, [i, j]]);
-        } else {
-          antennas.set(ch, [[i, j]]);
-        }
+        antennas.set(ch, has ? [...arr, [i, j]] : [[i, j]]);
       }
     }
   }
@@ -39,23 +35,16 @@ function compute(): number {
       for (let j = i + 1; j < positions.length; j++) {
         const a1 = positions[i];
         const a2 = positions[j];
-        const rowDiff = a1[0] - a2[0];
-        const colDiff = a1[1] - a2[1];
+        const [rowDiff, colDiff] = posSub(a1, a2);
+        const an1 = posAdd(a1, [rowDiff, colDiff]);
+        const an2 = posAdd(a2, [-rowDiff, -colDiff]);
 
-        const antiNode1: Pos = [a1[0] + rowDiff, a1[1] + colDiff];
-        const antiNode2: Pos = [a2[0] - rowDiff, a2[1] - colDiff];
-
-        const b1 = Mtr.isOnGrid(m, antiNode1);
-        const b2 = Mtr.isOnGrid(m, antiNode2);
-
-        if (b1) {
-          const key = JSON.stringify(antiNode1);
-          antinodes.add(key);
+        if (Mtr.isOnGrid(m, an1)) {
+          antinodes.add(Key(an1));
         }
 
-        if (b2) {
-          const key = JSON.stringify(antiNode2);
-          antinodes.add(key);
+        if (Mtr.isOnGrid(m, an2)) {
+          antinodes.add(Key(an2));
         }
       }
     }
@@ -73,25 +62,22 @@ function compute2(): number {
       for (let j = i + 1; j < positions.length; j++) {
         const a1 = positions[i];
         const a2 = positions[j];
-        const rowDiff = a1[0] - a2[0];
-        const colDiff = a1[1] - a2[1];
+        const [rowDiff, colDiff] = posSub(a1, a2);
 
         let ii = 0;
-        let an1: Pos = [a1[0] + ii * rowDiff, a1[1] + ii * colDiff];
+        let an1 = posSub(a1, [ii * rowDiff, ii * colDiff]);
         while (Mtr.isOnGrid(m, an1)) {
-          const key = JSON.stringify(an1);
-          antinodes.add(key);
+          antinodes.add(Key(an1));
           ii++;
-          an1 = [a1[0] + ii * rowDiff, a1[1] + ii * colDiff];
+          an1 = posSub(a1, [ii * rowDiff, ii * colDiff]);
         }
 
         ii = 0;
-        let an2: Pos = [a2[0] - ii * rowDiff, a2[1] - ii * colDiff];
+        let an2 = posSub(a2, [-ii * rowDiff, -ii * colDiff]);
         while (Mtr.isOnGrid(m, an2)) {
-          const key = JSON.stringify(an2);
-          antinodes.add(key);
+          antinodes.add(Key(an2));
           ii++;
-          an2 = [a2[0] - ii * rowDiff, a2[1] - ii * colDiff];
+          an2 = posSub(a2, [-ii * rowDiff, -ii * colDiff]);
         }
       }
     }

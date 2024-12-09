eb492644eb573e5b9d3f1d3df0a4f05173915aed
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-09 08:28:14 +0300

    remove only

--- src/app/day08/index.test.ts
+++ src/app/day08/index.test.ts
@@ -3,7 +3,7 @@ import { expect, test, describe } from "bun:test";
 
 const lines: string[] = readLines("day08/sample.txt");
 
-describe.only("day08 tests", () => {
+describe("day08 tests", () => {
   test("sample line length", () => {
     expect(lines.length).toBe(1);
   });

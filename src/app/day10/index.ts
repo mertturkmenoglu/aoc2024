f8e7545e69112b129b316251abeb2622d8bccf70
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-10 15:49:34 +0300

    uglify day 10

--- src/app/day10/index.ts
+++ src/app/day10/index.ts
@@ -1,20 +1,16 @@
 import { defineAocModule, Mtr, readLines, type Pos, cardinalCoefs as adj, posEq, posAdd, sum, type BfsNode } from "@/lib";
 
-let [L, K] = [readLines("day10/input.txt"), JSON.stringify];
-let [g, H] = [L.map((l) => l.split("").map(Number)), (a: N[], c: Pos) => a.some((x) => posEq(c, x.value))];
 type N = BfsNode<Pos>;
-let C = (q: N, path = [] as Pos[]): Pos[] => (q.parent === null ? path : C(q.parent, [...path, q.value]));
+let [g, H] = [readLines("day10/input.txt").map((l) => l.split("").map(Number)), (a: N[], c: Pos) => a.some((x) => posEq(c, x.value))];
+let [C, K] = [(q: N, path = [] as Pos[]): Pos[] => (q.parent === null ? path : C(q.parent, [...path, q.value])), JSON.stringify];
+let [Z, W] = [(q: N) => adj.map((a) => posAdd(q.value, a)), (U: Pos[], q: N) => U.map((c) => ({ value: c, parent: q }))];
+let X = (M: Pos[], S: boolean, D: N[], O: N[], v: number) => M.filter((c) => !((!S && H(D, c)) || H(O, c) || v! + 1 !== Mtr.$at(g, c)));
 
-function A(p: Pos, S = false): number {
-  let [P, O, D]: [Set<string>, N[], N[]] = [new Set(), [{ value: p, parent: null }], []];
-
+function A(p: Pos, S = false, P = new Set<string>(), O = [{ value: p, parent: null }] as N[], D = [] as N[]): number {
   while (O.length > 0) {
     let [v, _, q] = [Mtr.$at(g, O.at(-1)!.value), D.push(O.at(-1)!), O.pop()!];
-    [v].filter((x) => [9, undefined].includes(x)).map((x) => (x === 9 ? P.add(K(C(q))) : 0));
-    let Y = adj.map((a) => posAdd(q.value, a)).filter((c) => !((!S && H(D, c)) || H(O, c) || v! + 1 !== Mtr.$at(g, c)));
-    O.push(...Y.map((c) => ({ value: c, parent: q })));
+    [[v].filter((x) => [9, undefined].includes(x)).map((x) => (x === 9 ? P.add(K(C(q))) : 0)), O.push(...W(X(Z(q), S, D, O, v!), q))];
   }
-
   return P.size;
 }
 

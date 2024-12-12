61412919fceada9025cd82f58b60d71d8a7ab51a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-12 16:49:34 +0300

    uglify

--- src/app/day12/index.ts
+++ src/app/day12/index.ts
@@ -1,101 +1,38 @@
 import { defineAocModule, Mtr, readLines, type Pos, cardinalCoefs as adj, posAdd, posEq, sum } from "@/lib";
 
 const lines: string[] = readLines("day12/input.txt");
-const g = lines.map((l) => l.split(""));
-const go = lines.map((l) => l.split(""));
-let ptr = ((m = new Map<string, Region[]>()): Map<string, Region[]> => {
-  Mtr.forEachCell(g, (v, i, j) => {
-    if (v === ".") return;
-    let r = getRegion(v, [i, j]);
-    r.forEach((p) => Mtr.set(g, p, "."));
-    m.set(v, [...(m.get(v) ?? []), r]);
-  });
+let [K, V, Z] = [JSON.stringify, JSON.parse, (S: Set<string>) => [...S.entries()].map(([s]) => V(s) as [Pos, string])];
+let [g, go, H] = [lines.map((l) => l.split("")), lines.map((l) => l.split("")), (arr: Pos[], a: Pos) => arr.some((x) => posEq(x, a))];
+let P = (r: Pos[]) => sum(r.map((p) => 4 - adj.filter((dp) => Mtr.$at(go, posAdd(p, dp)) === Mtr.at(go, p)).length));
+let X = (r: Pos[], S = false) => r.length * (S ? N(r) : P(r));
+let D = (u: string[][], b: Pos[]) => b.forEach((p) => Mtr.set(u, p, "."));
+let E = (m: Map<string, Pos[][]>, v: string, p: Pos) => m.set(v, [...(m.get(v) ?? []), G(v, p)]).get(v)!;
+let A = (u: string[][], m = new Map<string, Pos[][]>()) => {
+  Mtr.forEachCell(u, (v, i, j) => (v !== "." ? D(u, E(m, v, [i, j]).at(-1)!) : {}));
   return m;
-})();
-
-type Region = Pos[];
-type BfsNode = {
-  value: Pos;
-  parent: BfsNode | null;
 };
+let w = (p: Pos, s: string): [Pos, string] => [p, s];
+let T = [w([0, -1], "L"), w([0, 1], "R"), w([1, 0], "T"), w([-1, 0], "B")];
 
-function getRegion(crop: string, sp: Pos, open = [{ value: sp, parent: null }] as BfsNode[]): Region {
-  let close: BfsNode[] = [];
-  let reg: Pos[] = [];
-
-  while (open.length > 0) {
-    let q = open.shift()!;
-    close.push(q);
-    if (Mtr.$at(g, q.value) === crop) {
-      reg.push(q.value);
-    }
-    let has = (arr: BfsNode[], a: Pos) => arr.some((x) => posEq(x.value, a));
-    let A = adj.map((p) => posAdd(q.value, p)).filter((c) => !(has(close, c) || has(open, c) || Mtr.$at(g, c) !== crop));
-    open.push(...A.map((c) => ({ value: c, parent: q })));
+function G(C: string, S: Pos, O = [S] as Pos[], L = [] as Pos[], R = [] as Pos[]): Pos[] {
+  while (O.length > 0) {
+    let [_, q] = [L.push(O[0]), O.shift()!];
+    R.push(...[q].filter((x) => Mtr.$at(g, x) === C));
+    O.push(...adj.map((p) => posAdd(q, p)).filter((c) => !(H(L, c) || H(O, c) || Mtr.$at(g, c) !== C)));
   }
-
-  return reg;
+  return R;
 }
 
-let price = (r: Region, S = false) => r.length * (S ? sides(r) : perimeter(r));
-
-function sides(region: Region): number {
-  let candidates: Pos[] = [];
-
-  for (let [r, c] of region) {
-    let tmp: Pos[] = [
-      [r - 0.5, c - 0.5],
-      [r + 0.5, c - 0.5],
-      [r + 0.5, c + 0.5],
-      [r - 0.5, c + 0.5],
-    ];
-
-    for (let [cr, cc] of tmp) {
-      if (!candidates.some((x) => posEq(x, [cr, cc]))) {
-        candidates.push([cr, cc]);
-      }
-    }
-  }
-
-  let corners = 0;
-
-  for (let [cr, cc] of candidates) {
-    let cfg: boolean[] = [];
-    let tmp: Pos[] = [
-      [cr - 0.5, cc - 0.5],
-      [cr + 0.5, cc - 0.5],
-      [cr + 0.5, cc + 0.5],
-      [cr - 0.5, cc + 0.5],
-    ];
-    for (let [sr, sc] of tmp) {
-      cfg.push(region.some((x) => posEq(x, [sr, sc])));
-    }
-    let num = cfg.filter((x) => x).length;
-    if (num === 1) {
-      corners += 1;
-    } else if (num === 2) {
-      let K = JSON.stringify;
-      let a1 = K([true, false, true, false]);
-      let a2 = K([false, true, false, true]);
-      let c = K(cfg);
-
-      if (c === a1 || c === a2) {
-        corners += 2;
-      }
-    } else if (num === 3) {
-      corners += 1;
-    }
-  }
-
-  return corners;
+function N(n: Pos[]): number {
+  let S = new Set(n.flatMap((p) => T.filter(([dp]) => Mtr.$at(go, posAdd(p, dp)) !== Mtr.at(go, p)).map(([_, dir]) => K([p, dir]))));
+  Z(S).forEach(([[r, c], dir]) => [K([[r, c - 1], dir]), K([[r - 1, c], dir])].forEach((k) => (S.has(k) ? S.delete(k) : {})));
+  return S.size;
 }
-
-let perimeter = (r: Region) => sum(r.map((p) => 4 - adj.filter((dp) => Mtr.$at(go, posAdd(p, dp)) === Mtr.at(go, p)).length));
 
 export default defineAocModule({
   day: 12,
   exp1: 1_485_656,
   exp2: 899_196,
-  sol1: () => sum([...ptr.values()].flatMap((rs) => rs.map((r) => price(r)))),
-  sol2: () => sum([...ptr.values()].flatMap((rs) => rs.map((r) => price(r, true)))),
+  sol1: () => sum([...A(V(K(g))).values()].flatMap((rs) => rs.map((r) => X(r)))),
+  sol2: () => sum([...A(V(K(g))).values()].flatMap((rs) => rs.map((r) => X(r, true)))),
 });

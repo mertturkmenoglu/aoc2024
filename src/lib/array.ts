ae0f63650d090e5bb5b7e20758c2a2d3d2c83f15
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-13 09:23:19 +0300

    update array range utility

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -22,9 +22,9 @@ export function repeat<T>(n: number, el: T): T[] {
   return new Array(n).fill(el);
 }
 
-export function range(start: number, end: number): number[] {
+export function range(start: number, end: number, step = 1): number[] {
   const arr: number[] = [];
-  for (let i = start; i < end; i++) {
+  for (let i = start; i < end; i += step) {
     arr.push(i);
   }
   return arr;

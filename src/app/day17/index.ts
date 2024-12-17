a92553497acd3b4dd7ca379c1ea1572db3bca4aa
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-17 09:51:51 +0300

    code cleanup

--- src/app/day17/index.ts
+++ src/app/day17/index.ts
@@ -9,56 +9,30 @@ function compute(R?: bigint): string {
   let output: bigint[] = [];
   ra = R !== undefined ? R : ra;
 
-  let getCombo = (op: bigint): bigint => {
+  let C = (op: bigint): bigint => {
+    if (op === 7n) throw new Error("combo operand 7");
     if (op >= 0n && op <= 3n) return BigInt(op);
-    if (op === 4n) return ra;
-    if (op === 5n) return rb;
-    if (op === 6n) return rc;
-    throw new Error("combo operand 7");
+    return [ra, rb, rc][Number(op - 4n)];
   };
 
   while (ip < program.length) {
-    let ins = program[ip];
-    let operand = program[ip + 1];
+    let [I, op] = [program[ip], program[ip + 1]];
 
-    if (ins === 0n) {
-      let c = getCombo(operand);
-      ra = ra / 2n ** c;
-      ip += 2;
-    } else if (ins === 1n) {
-      rb = rb ^ operand;
-      ip += 2;
-    } else if (ins === 2n) {
-      let c = getCombo(operand);
-      rb = c % 8n;
-      ip += 2;
-    } else if (ins === 3n) {
-      if (ra === 0n) {
-        ip += 2;
-      } else {
-        ip = Number(operand);
-      }
-    } else if (ins === 4n) {
-      rb = rb ^ rc;
-      ip += 2;
-    } else if (ins === 5n) {
-      let c = getCombo(operand);
-      output.push(c % 8n);
+    if (I === 0n) ra /= 2n ** C(op);
+    else if (I === 1n) rb ^= op;
+    else if (I === 2n) rb = C(op) % 8n;
+    else if (I === 3n && ra !== 0n) ip = Number(op) - 2;
+    else if (I === 4n) rb ^= rc;
+    else if (I === 6n) rb = ra / 2n ** C(op);
+    else if (I === 7n) rc = ra / 2n ** C(op);
+    else if (I === 5n) {
+      output.push(C(op) % 8n);
       if (R !== undefined && !program.join(",").startsWith(output.join(","))) {
         return output.join(",");
       }
-      ip += 2;
-    } else if (ins === 6n) {
-      let c = getCombo(operand);
-      rb = ra / 2n ** c;
-      ip += 2;
-    } else if (ins === 7n) {
-      let c = getCombo(operand);
-      rc = ra / 2n ** c;
-      ip += 2;
-    } else {
-      throw new Error("unknown instruction" + ins);
     }
+
+    ip += 2;
   }
 
   return output.join(",");

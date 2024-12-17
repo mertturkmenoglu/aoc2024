9e650285f602fcd2650c52f42c0d1b5370470606
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-17 10:05:38 +0300

    update type defs

--- src/lib/measure.ts
+++ src/lib/measure.ts
@@ -1,8 +1,8 @@
-type Result = number;
+type Result = number | string;
 
 type Duration = number;
 
-export function measure(fn: () => number): [Result, Duration] {
+export function measure(fn: () => number | string): [Result, Duration] {
   const start = performance.now();
   const res = fn();
   const end = performance.now();

5b83bbe1971b285373ed341a502e2529b95f34de
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-18 08:55:01 +0300

    cleanup

--- src/app/day18/index.ts
+++ src/app/day18/index.ts
@@ -1,37 +1,25 @@
-import { defineAocModule, Mtr, nums, posEq, readLines, type Pos, cardinalCoefs as adj, posAdd, type BfsNode } from "@/lib";
+import { defineAocModule, Mtr, nums, posEq, readLines, type Pos, cardinalCoefs as adj, posAdd, type BfsNode, Arr } from "@/lib";
 
-let [bytes, g] = [readLines("day18/input.txt").map((x) => nums(x).toReversed() as Pos), Mtr.createMtr([71, 71], ".")];
+let [B, g] = [readLines("day18/input.txt").map((x) => nums(x).toReversed() as Pos), Mtr.createMtr([71, 71], ".")];
 
 type N = BfsNode<Pos>;
 
-function constructPath(end: N | null, path = [] as Pos[]): Pos[] {
-  return end === null ? path.reverse() : constructPath(end.parent, [...path, end.value]);
+function C(end: N | null, path = [] as Pos[]): Pos[] {
+  return end === null ? path.reverse() : C(end.parent, [...path, end.value]);
 }
 
-function bfs(): Pos[] {
-  let open: N[] = [{ value: [0, 0], parent: null }];
-  let closed: N[] = [];
-
+function bfs(open = [{ value: [0, 0], parent: null }] as N[], closed = [] as N[]): Pos[] {
   while (open.length > 0) {
-    const q = open.shift()!;
-    closed.push(q);
+    let [, q] = [closed.push(open[0]), open.shift()!];
 
     if (posEq(q.value, [70, 70])) {
-      let path = constructPath(q);
-      return path;
+      return C(q);
     }
 
-    for (let child of adj.map((x) => posAdd(q.value, x))) {
-      if (
-        !Mtr.isOnGrid(g, child) ||
-        Mtr.at(g, child) === "#" ||
-        closed.some((x) => posEq(x.value, child)) ||
-        open.some((x) => posEq(x.value, child))
-      ) {
-        continue;
+    for (let child of adj.map((x) => posAdd(q.value, x)).filter((x) => Mtr.isOnGrid(g, x) && Mtr.at(g, x) !== "#")) {
+      if (!closed.some((x) => posEq(x.value, child)) && !open.some((x) => posEq(x.value, child))) {
+        open.push({ parent: q, value: child });
       }
-
-      open.push({ parent: q, value: child });
     }
   }
 
@@ -40,9 +28,7 @@ function bfs(): Pos[] {
 
 let fill = (n: number) => {
   g = Mtr.createMtr([71, 71], ".");
-  for (let i = 0; i < n; i++) {
-    Mtr.set(g, bytes[i], "#");
-  }
+  Arr.range(0, n).forEach((i) => Mtr.set(g, B[i], "#"));
 };
 
 function sol1(): number {
@@ -50,24 +36,18 @@ function sol1(): number {
   return bfs().length - 1;
 }
 
-function sol2(): string {
+function sol2(lo = 0, hi = B.length): string {
   fill(2024);
-  let [lo, hi] = [0, bytes.length];
 
   while (lo < hi) {
     let m = Math.floor(lo + (hi - lo) / 2);
     fill(m);
-    let path = bfs();
 
-    if (path.length === 0) {
-      hi = m;
-    } else {
-      lo = m + 1;
-    }
+    if (bfs().length === 0) hi = m;
+    else lo = m + 1;
   }
 
-  let i = lo - 1;
-  return `${bytes[i][1]},${bytes[i][0]}`;
+  return `${B[lo - 1][1]},${B[lo - 1][0]}`;
 }
 
 export default defineAocModule({

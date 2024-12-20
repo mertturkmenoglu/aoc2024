cb31c050078ed1c9813e541a1ccfc0d151b70df4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-20 09:14:58 +0300

    add day 20 part 1 and part 2

--- src/app/day20/index.ts
+++ src/app/day20/index.ts
@@ -1,19 +1,26 @@
-import { defineAocModule, readLines } from "@/lib";
+import { defineAocModule, Mtr, readLines, Graph } from "@/lib";
 
-const lines: string[] = readLines("day20/input.txt");
+let g = readLines("day20/input.txt").map((x) => x.split(""));
+let [start, end] = [Mtr.find(g, "S")!, Mtr.find(g, "E")!];
+let path = Graph.bfs(g, start, end);
 
-function sol1(): number {
-  return 0;
-}
-
-function sol2(): number {
-  return 0;
+function compute1(s1 = true): number {
+  let sum = 0;
+  for (let i = 0; i < path.length; i++) {
+    for (let j = i + 102; j < path.length; j++) {
+      let dist = Math.abs(path[j][0] - path[i][0]) + Math.abs(path[j][1] - path[i][1]);
+      if ((s1 && dist === 2) || (!s1 && dist <= 20 && j - i - dist >= 100)) {
+        sum++;
+      }
+    }
+  }
+  return sum;
 }
 
 export default defineAocModule({
   day: 20,
-  exp1: 0,
-  exp2: 0,
-  sol1,
-  sol2,
+  exp1: 1286,
+  exp2: 989_316,
+  sol1: () => compute1(),
+  sol2: () => compute1(false),
 });

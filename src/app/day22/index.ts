264242c2a873ac6a063b14e004b8d371b169c7ec
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-22 08:32:45 +0300

    uglify

--- src/app/day22/index.ts
+++ src/app/day22/index.ts
@@ -1,23 +1,8 @@
 import { defineAocModule, readLines } from "@/lib";
 
-const lines: string[] = readLines("day22/input.txt");
-
-function compute1(): bigint {
-  let [sum, prune] = [0n, 16_777_216n];
-  for (let n of lines.map(BigInt)) {
-    for (let i = 0; i < 2000; i++) {
-      n = ((n * 64n) ^ n) % prune;
-      n = ((n / 32n) ^ n) % prune;
-      n = ((n * 2048n) ^ n) % prune;
-    }
-    sum += n;
-  }
-  return sum;
-}
-
-function sol1(): bigint {
-  return compute1();
-}
+let [N, P, M] = [readLines("day22/input.txt").map((x) => BigInt(x)), 16_777_216n, (n: bigint) => ((n * 64n) ^ n) % P];
+let [D, O] = [(n: bigint) => ((n / 32n) ^ n) % P, (n: bigint) => ((n * 2048n) ^ n) % P];
+let C = (n: bigint, i = 0): bigint => (i >= 2000 ? n : C(O(D(M(n))), i + 1));
 
 function sol2(): number {
   return 0;
@@ -27,6 +12,6 @@ export default defineAocModule({
   day: 22,
   exp1: 17_965_282_217n,
   exp2: 0,
-  sol1,
+  sol1: () => N.map((x) => C(x)).reduce((acc, x) => acc + x, 0n),
   sol2,
 });

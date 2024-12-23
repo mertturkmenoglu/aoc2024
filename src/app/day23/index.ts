189e9a0a73ddb9c9ff2155d4b21268b29b5f52db
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-23 11:54:08 +0300

    uglify

--- src/app/day23/index.ts
+++ src/app/day23/index.ts
@@ -1,130 +1,69 @@
 import { defineAocModule, readLines } from "@/lib";
 
 const lines: string[] = readLines("day23/input.txt");
+let inp = lines.map((l) => l.split("-") as [string, string]);
+let [K, P] = [JSON.stringify, JSON.parse];
 
-function parseInput() {
-  let rels: [string, string][] = [];
-  for (let line of lines) {
-    let [a, b] = line.split("-");
-    rels.push([a, b]);
-  }
-  return rels;
-}
+let getConnections = () => {
+  let c = new Map<string, Set<string>>();
 
-function comp1(relations: [string, string][]): number {
-  let connections = new Map<string, Set<string>>();
-
-  for (let [a, b] of relations) {
-    if (connections.has(a)) {
-      connections.set(a, connections.get(a)!.add(b));
-    } else {
-      connections.set(a, new Set<string>().add(b));
-    }
-
-    if (connections.has(b)) {
-      connections.set(b, connections.get(b)!.add(a));
-    } else {
-      connections.set(b, new Set<string>().add(a));
-    }
+  for (let [a, b] of inp) {
+    if (!c.has(a)) c.set(a, new Set([b]));
+    if (!c.has(b)) c.set(b, new Set([a]));
+    c.set(a, c.get(a)!.add(b));
+    c.set(b, c.get(b)!.add(a));
   }
 
+  return c;
+};
+
+function comp1(): number {
+  let c = getConnections();
   let sets = new Set<string>();
 
-  for (let [x] of connections) {
-    for (let y of connections.get(x)!) {
-      for (let z of connections.get(y)!) {
-        if (x !== z && connections.get(z)!.has(x)) {
-          sets.add(JSON.stringify([x, y, z].toSorted()));
+  for (let [x, s] of c) {
+    for (let y of s) {
+      for (let z of c.get(y)!) {
+        if (x !== z && c.get(z)!.has(x)) {
+          sets.add(K([x, y, z].toSorted()));
         }
       }
     }
   }
 
-  let counter = 0;
-
-  for (let s of sets) {
-    let set: string[] = JSON.parse(s);
-    if (set.some((x) => x.startsWith("t"))) {
-      counter++;
-    }
-  }
-
-  return counter;
+  return [...sets.values()].map((s) => (P(s) as string[]).some((x) => x.startsWith("t"))).filter(Boolean).length;
 }
 
-function comp2(relations: [string, string][]): string {
-  let connections = new Map<string, Set<string>>();
-
-  for (let [a, b] of relations) {
-    if (connections.has(a)) {
-      connections.set(a, connections.get(a)!.add(b));
-    } else {
-      connections.set(a, new Set<string>().add(b));
-    }
-
-    if (connections.has(b)) {
-      connections.set(b, connections.get(b)!.add(a));
-    } else {
-      connections.set(b, new Set<string>().add(a));
-    }
-  }
-
+function comp2(): string {
+  let c = getConnections();
   let sets = new Set<string>();
 
   let search = (node: string, required: Set<string>) => {
-    let key = JSON.stringify([...required.values()].toSorted());
-    if (sets.has(key)) {
-      return;
-    }
+    let key = K([...required.values()].toSorted());
+    if (sets.has(key)) return;
 
     sets.add(key);
 
-    for (let neighbour of connections.get(node)!) {
-      if (required.has(neighbour)) {
-        continue;
-      }
-
-      if (![...required.values()].every((q) => connections.get(q)!.has(neighbour))) {
-        continue;
-      }
-
-      search(neighbour, new Set<string>([...required.values(), neighbour]));
+    for (let n of c.get(node)!) {
+      if (required.has(n)) continue;
+      if (![...required.values()].every((q) => c.get(q)!.has(n))) continue;
+      search(n, new Set<string>([...required.values(), n]));
     }
   };
 
-  for (let [x] of connections) {
-    search(x, new Set([x]));
-  }
+  c.forEach(([x]) => search(x, new Set([x])));
 
-  let arr = [...sets.values()].map((x) => JSON.parse(x) as string[]);
-
-  let len = -1;
-  let ind = -1;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].length > len) {
-      len = arr[i].length;
-      ind = i;
-    }
-  }
-
-  return arr[ind].toSorted().join(",");
-}
-
-function sol1(): number {
-  let inp = parseInput();
-  return comp1(inp);
-}
-
-function sol2(): string {
-  let inp = parseInput();
-  return comp2(inp);
+  return [...sets.values()]
+    .map((x) => P(x) as string[])
+    .toSorted((a, b) => b.length - a.length)[0]
+    .toSorted()
+    .join(",");
 }
 
 export default defineAocModule({
   day: 23,
   exp1: 1337,
-  exp2: "-",
-  sol1,
-  sol2,
+  exp2: "aw,fk,gv,hi,hp,ip,jy,kc,lk,og,pj,re,sr",
+  sol1: () => comp1(),
+  sol2: () => comp2(),
 });

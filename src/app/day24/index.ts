430e040607372e4572a169361ee6c1eec2b1ec7d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-24 08:39:59 +0300

    uglify

--- src/app/day24/index.ts
+++ src/app/day24/index.ts
@@ -9,20 +9,15 @@ type Input = {
 
 function parse(): Input {
   let [fst, snd] = lines.join("\n").split("\n\n");
-  let initials = fst.split("\n").map((line) => {
-    let parts = line.split(": ");
-    return [parts[0], +parts[1]] as [string, number];
-  });
-  let a = snd.split("\n");
-  let b = a.map((x) => x.split(" -> "));
-  let out: [string, string, string, string][] = [];
-  for (let [c, d] of b) {
-    let [gate1, op, gate2] = c.split(" ");
-    out.push([gate1, op, gate2, d]);
-  }
   return {
-    initials,
-    steps: out,
+    initials: fst
+      .split("\n")
+      .map((line) => line.split(": ") as [string, string])
+      .map(([x, y]) => [x, +y] as [string, number]),
+    steps: snd
+      .split("\n")
+      .map((x) => x.split(" -> "))
+      .map(([c, d]) => [...(c.split(" ") as [string, string, string]), d]),
   };
 }
 
@@ -32,37 +27,20 @@ function comp1(inp: Input): number {
     let j = 0;
     for (j; j < inp.steps.length; j++) {
       let [gate1, _op, gate2] = inp.steps[j];
-      let g1 = values.find((x) => x[0] === gate1);
-      let g2 = values.find((x) => x[0] === gate2);
-      if (g1 !== undefined && g2 !== undefined) {
+      if (values.find((x) => x[0] === gate1) !== undefined && values.find((x) => x[0] === gate2) !== undefined) {
         break;
       }
     }
     let [[gate1, op, gate2, out]] = inp.steps.splice(j, 1);
-    let g1 = values.find((x) => x[0] === gate1)![1];
-    let g2 = values.find((x) => x[0] === gate2)![1];
-    let val = 0;
-    if (op === "AND") {
-      val = g1 & g2;
-    } else if (op === "OR") {
-      val = g1 | g2;
-    } else if (op === "XOR") {
-      val = g1 ^ g2;
-    }
+    let [g1, g2, val] = [values.find((x) => x[0] === gate1)![1], values.find((x) => x[0] === gate2)![1], 0];
+    if (op === "AND") val = g1 & g2;
+    else if (op === "OR") val = g1 | g2;
+    else if (op === "XOR") val = g1 ^ g2;
     values.push([out, val]);
   }
-  let zGates = values
-    .filter((x) => x[0].startsWith("z"))
-    .toSorted()
-    .toReversed()
-    .map((x) => x[1])
-    .join("");
-  return parseInt(zGates, 2);
-}
-
-function sol1(): number {
-  let inp = parse();
-  return comp1(inp);
+  let zGates = values.filter((x) => x[0].startsWith("z"));
+  let sorted = zGates.toSorted().toReversed();
+  return parseInt(sorted.map((x) => x[1]).join(""), 2);
 }
 
 function sol2(): number {
@@ -71,8 +49,8 @@ function sol2(): number {
 
 export default defineAocModule({
   day: 24,
-  exp1: 0,
+  exp1: 56_620_966_442_854,
   exp2: 0,
-  sol1,
+  sol1: () => comp1(parse()),
   sol2,
 });
